442b6d40bfa6e819ce95a0e795f3b801492f7de0
jin <jin@hyoo.ru>
2021-05-07 11:03:48 +0300

    return actual on put

--- server.js
+++ server.js
@@ -2,134 +2,184 @@ const http = require('http')
 const express = require('express')
 const cors = require('cors')
 const ws = require('ws')
+const { Pool } = require('pg')
 
-const router = express()
-const server = http.createServer( router )
-
-router.use( cors() )
-router.use( express.json() )
-
-const socket = new ws.Server({
-  server,
-  perMessageDeflate: {
-    zlibDeflateOptions: {
-      chunkSize: 1024,
-      memLevel: 7,
-      level: 3
-    },
-    zlibInflateOptions: {
-      chunkSize: 10 * 1024
-    },
-  }
-})
-
-
-const store = new Map
-
-/** Returns room attached to origin */
-function Room( origin ) {
-
-  let room = store.get( origin )
+const main = async() => {
   
-  if( !room ) {
-    store.set( origin, room = {
-      state: new Map,
-      watch: new Map,
+  const db = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  })
+  await db.connect()
+  
+  await db.query(`
+    CREATE TABLE IF NOT EXISTS store (
+      key         varchar(255) UNIQUE NOT NULL,
+      value       jsonb
+    );
+  `)
+  
+  const router = express()
+  const server = http.createServer( router )
+
+  router.use( cors() )
+  router.use( express.json() )
+
+  const socket = new ws.Server({
+    server,
+    perMessageDeflate: {
+      zlibDeflateOptions: {
+        chunkSize: 1024,
+        memLevel: 7,
+        level: 3
+      },
+      zlibInflateOptions: {
+        chunkSize: 10 * 1024
+      },
+    }
+  })
+
+
+  const store = new Map
+
+  /** Returns room attached to origin */
+  function Room( origin ) {
+
+    let room = store.get( origin )
+    
+    if( !room ) {
+      store.set( origin, room = {
+        watch: new Map,
+      } )
+    }
+    
+    return room
+  }
+
+  /** Returns value by keys and subscribes line to value cahnges */
+  async function get( origin, key, line ) {
+
+    if( line ) {
+
+      const room = Room( origin )
+
+      let keys = room.watch.get( line )
+      if( !keys ) room.watch.set( line, keys = new Set )
+      
+      keys.add( key )
+    }
+    
+    const res = await db.query(`SELECT value FROM store WHERE key = $1::text`, [ origin + '/' + key ] )
+    return res.rows[0] ? res.rows[0].value : null
+  }
+
+  /** Unsubscribes line from all keys */
+  function forget( origin, line ) {
+    const room = Room( origin )
+    room.watch.delete( line )
+  }
+
+  /** Put value by key and notify all subscribed lines except current */
+  async function put( origin, key, val, line ) {
+
+    const room = Room( origin )
+    const prev = await get( origin, key ) || {}
+    val = Object.assign( prev, val )
+    
+    const res = await db.query(
+      `
+      INSERT INTO store ( key, value )
+      VALUES( $1::text, $2::json )
+      ON CONFLICT( key ) DO UPDATE
+      SET value = $2::json;
+      `,
+      [ origin + '/' + key, val ]
+    )
+
+    for( const [ other, keys ] of room.watch ) {
+      if( line === other ) continue
+      if( !keys.has( key ) ) continue
+      other.send( JSON.stringify([ key, val ]) )
+    }
+    
+    return val
+  }
+
+  /** GET /key */
+  router.get( '*', async( req, res )=> {
+    const origin = req.headers.origin || req.protocol + '://' + req.headers.host
+    res.set( 'Content-Type', 'application/json' )
+    res.send( JSON.stringify( await get( origin, req.url.slice(1) ) || null ) )
+  } )
+
+  /** PUT /key */
+  router.put( '*', async( req, res )=> {
+    const origin = req.headers.origin || req.protocol + '://' + req.headers.host
+    res.set( 'Content-Type', 'application/json' )
+    res.send( JSON.stringify( await put( origin, req.url.slice(1), req.body ) || null ) )
+  } )
+
+  /**
+   * Get & Subscribe: [ key ]
+   * Put | Notification: [ key, patch ]
+   * Unsubscribe: disconnect
+   */
+  socket.on( 'connection' , ( line, req )=> {
+
+    const origin = req.headers.origin
+
+    line
+    .on( 'message' , async( message )=> {
+      
+      try {
+        message = JSON.parse( message )
+      } catch( error ) {
+        console.error( error )
+        return
+      }
+
+      if( !Array.isArray( message ) ) return
+
+      const [ key, val ] = message
+
+      if( val ) {
+        line.send(
+          JSON.stringify([
+            key,
+            await put( origin, key, val, line ),
+          ])
+        )
+      } else {
+        line.send(
+          JSON.stringify([
+            key,
+            await get( origin, key, line ),
+          ])
+        )
+      }
+
     } )
-  }
-  
-  return room
-}
-
-/** Returns value by keys and subscribes line to value cahnges */
-function get( origin, key, line ) {
-
-  const room = Room( origin )
-
-  if( line ) {
-
-    let keys = room.watch.get( line )
-    if( !keys ) room.watch.set( line, keys = new Set )
-    
-    keys.add( key )
-  }
-  
-  return room.state.get( key )
-}
-
-/** Unsubscribes line from all keys */
-function forget( origin, line ) {
-  const room = Room( origin )
-  room.watch.delete( line )
-}
-
-/** Put value by key and notify all subscribed lines except current */
-function put( origin, key, val, line ) {
-
-  const room = Room( origin )
-
-  val = Object.assign( room.state.get( key ) || {}, val )
-  room.state.set( key, val )
-
-  for( const [ other, keys ] of room.watch ) {
-    if( line === other ) continue
-    if( !keys.has( key ) ) continue
-    other.send( JSON.stringify([ key, val ]) )
-  }
-  
-  return val
-}
-
-/** GET /key */
-router.get( '*', ( req, res )=> {
-  res.set( 'Content-Type', 'application/json' )
-  res.send( JSON.stringify( get( req.headers.origin, req.url.slice(1) ) || null ) )
-} )
-
-/** PUT /key */
-router.put( '*', ( req, res )=> {
-  res.set( 'Content-Type', 'application/json' )
-  res.send( JSON.stringify( put( req.headers.origin, req.url.slice(1), req.body ) || null ) )
-} )
-
-/**
- * Get & Subscribe: [ key ]
- * Put | Notification: [ key, patch ]
- * Unsubscribe: disconnect
- */
-socket.on( 'connection' , ( line, req )=> {
-
-  const origin = req.headers.origin
-
-  line
-  .on( 'message' , message => {
-    
-    try {
-      message = JSON.parse( message )
-    } catch( error ) {
-      console.error( error )
-      return
-    }
-
-    if( !Array.isArray( message ) ) return
-
-    const [ key, val ] = message
-
-    if( val ) {
-      put( origin, key, val, line )
-    } else {
-      line.send( JSON.stringify([ key, get( origin, key, line ) ]) )
-    }
+    .on( 'close', ( code, reason )=> {
+      forget( origin, line )
+    } )
 
   } )
-  .on( 'close', ( code, reason )=> {
-    forget( origin, line )
+
+
+  server.listen( process.env.PORT || 3000, process.env.IP || "0.0.0.0", ()=> {
+    var addr = server.address()
+    console.log( "Server listening at ", addr.address + ":" + addr.port )
   } )
 
-} )
+}
 
+const start = async ()=> {
+  try {
+    await main()
+  } catch( error ) {
+    console.error( error )
+    process.exit(1)
+  }
+}
 
-server.listen( process.env.PORT || 3000, process.env.IP || "0.0.0.0", ()=> {
-  var addr = server.address()
-  console.log( "Server listening at ", addr.address + ":" + addr.port )
-} )
+start()

eb284908c984d901e61d7a4ee56b0e94bb065d6e
jin <jin@hyoo.ru>
2022-08-26 18:50:18 +0300

    +cors

--- server/server.ts
+++ server/server.ts
@@ -19,7 +19,10 @@ namespace $ {
 				
 				const query = $hyoo_harp_from_string( query_str )
 				if( !query.land ) {
-					res.writeHead( 200, { 'Content-Type': 'text/plain;charset=utf-8' } )
+					res.writeHead( 200, {
+						'Content-Type': 'text/plain;charset=utf-8',
+						'Access-Control-Allow-Origin': '*',
+					} )
 					res.end( '$hyoo_sync_server ✅' )
 					return
 				}
@@ -97,7 +100,10 @@ namespace $ {
 					land: reply,
 				}
 				
-				res.writeHead( 200, { 'Content-Type': 'application/json' } )
+				res.writeHead( 200, {
+					'Content-Type': 'application/json',
+					'Access-Control-Allow-Origin': '*',
+				} )
 				res.end( JSON.stringify( response, null, '\t' ) )
 				
 			} )

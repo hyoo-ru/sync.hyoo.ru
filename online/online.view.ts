4be7208f27a6ea276da1fd3fa1be18682296e4ca
jin <jin@hyoo.ru>
2022-08-28 20:22:07 +0300

    Online status view

---
+++ online/online.view.ts
@@ -0,0 +1,41 @@
+namespace $.$$ {
+	export class $hyoo_sync_online extends $.$hyoo_sync_online {
+		
+		@ $mol_mem
+		message() {
+			
+			try {
+				
+				this.online()
+				return this.hint()
+			
+			} catch( error ) {
+				if( error instanceof Promise ) $mol_fail_hidden( error )
+				
+				$mol_fail_log( error )
+				return String( error )
+				
+			}
+			
+		}
+		
+		@ $mol_mem
+		sub() {
+			
+			try {
+				
+				this.online()
+				return [ this.Well() ]
+			
+			} catch( error ) {
+				if( error instanceof Promise ) $mol_fail_hidden( error )
+				
+				$mol_fail_log( error )
+				return [ this.Fail() ]
+				
+			}
+			
+		}
+		
+	}
+}

b91915d7b2eb00e9bc429c605eb7c5c98a7fadcd
nin-jin <nin-jin@users.noreply.github.com>
2022-09-17 17:37:53 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -24,7 +24,7 @@ module.exports = $;
 //hyoo/hyoo.ts
 ;
 "use strict";
-let $hyoo_sync_revision = "3f91442";
+let $hyoo_sync_revision = "a1f5833";
 //hyoo/sync/-meta.tree/revision.meta.tree.ts
 ;
 "use strict";

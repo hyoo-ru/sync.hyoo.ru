cd518d133efaa2ec11fa07c6baed6b86eaaa86d4
jin <jin@hyoo.ru>
2022-09-26 21:30:07 +0300

    fix auth

--- auth/auth.ts
+++ auth/auth.ts
@@ -6,13 +6,13 @@ namespace $ {
 		if( event.data[0] !== '$hyoo_sync_peer' ) return
 		
 		if( $hyoo_sync_origins.every( domain => !event.origin.endsWith( domain ) ) ) {
-			return event.source!.postMessage( event.data )
+			return event.source!.postMessage( event.data, { targetOrigin: '*' } )
 		}
 		
 		let key = $mol_state_local.value( '$hyoo_sync_peer' ) as string | null
 		if( !key ) $mol_state_local.value( '$hyoo_sync_peer', key = event.data[1] )
 		
-		event.source!.postMessage([ '$hyoo_sync_peer', key ])
+		event.source!.postMessage( [ '$hyoo_sync_peer', key ], { targetOrigin: '*' } )
 		
 	} )
 	

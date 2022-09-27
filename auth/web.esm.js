2f44faf60c3aa46928a35e60861db564a30370ab
nin-jin <nin-jin@users.noreply.github.com>
2022-09-27 09:11:03 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- auth/web.esm.js
+++ auth/web.esm.js
@@ -32,7 +32,7 @@ $node[ "../mam.ts" ] = $node[ "../mam.ts" ] = module.exports }.call( {} , {} )
 //hyoo/hyoo.ts
 ;
 "use strict";
-let $hyoo_sync_revision = "07aaa6d";
+let $hyoo_sync_revision = "f131b17";
 //hyoo/sync/-meta.tree/revision.meta.tree.ts
 ;
 "use strict";

9c8f7a9feca870ca04d8753f07df718c43ad2e0c
nin-jin <nin-jin@users.noreply.github.com>
2022-09-27 09:04:37 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.js
+++ node.js
@@ -32,7 +32,7 @@ $node[ "../mam.ts" ] = $node[ "../mam.ts" ] = module.exports }.call( {} , {} )
 //hyoo/hyoo.ts
 ;
 "use strict";
-let $hyoo_sync_revision = "3e987ed";
+let $hyoo_sync_revision = "07aaa6d";
 //hyoo/sync/-meta.tree/revision.meta.tree.ts
 ;
 "use strict";

c2cc5b19036af8d02e7abc7fc03a3c506075593a
nin-jin <nin-jin@users.noreply.github.com>
2023-02-12 07:16:56 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- auth/node.esm.js
+++ auth/node.esm.js
@@ -1333,9 +1333,12 @@ var $;
 "use strict";
 var $;
 (function ($) {
+    const named = new WeakSet();
     function $mol_func_name(func) {
         let name = func.name;
         if (name?.length > 1)
+            return name;
+        if (named.has(func))
             return name;
         for (let key in this) {
             try {
@@ -1347,6 +1350,7 @@ var $;
             }
             catch { }
         }
+        named.add(func);
         return name;
     }
     $.$mol_func_name = $mol_func_name;

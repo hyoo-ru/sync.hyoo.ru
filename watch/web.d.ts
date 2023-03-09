804fdc1644458a86b57ed8d210c1b79cf86cf726
nin-jin <nin-jin@users.noreply.github.com>
2023-03-09 07:18:32 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- watch/web.d.ts
+++ watch/web.d.ts
@@ -3423,23 +3423,6 @@ declare namespace $ {
 }
 
 declare namespace $ {
-    class $mol_ghost extends $mol_view {
-        Sub(): $mol_view;
-    }
-}
-
-declare namespace $.$$ {
-    class $mol_ghost extends $.$mol_ghost {
-        dom_node(next?: Element): Element;
-        dom_node_actual(): Element;
-        dom_tree(): Element;
-        title(): string;
-        minimal_width(): number;
-        minimal_height(): number;
-    }
-}
-
-declare namespace $ {
     class $mol_embed_native extends $mol_scroll {
         uri(val?: any): string;
         dom_name(): string;
@@ -3535,7 +3518,7 @@ declare namespace $ {
 }
 
 declare namespace $ {
-    class $mol_embed_any extends $mol_ghost {
+    class $mol_embed_any extends $mol_view {
         Image(): $mol_image;
         Object(): $$.$mol_embed_native;
         Youtube(): $$.$mol_embed_youtube;
@@ -3547,7 +3530,7 @@ declare namespace $ {
 declare namespace $.$$ {
     class $mol_embed_any extends $.$mol_embed_any {
         type(): "image" | "object" | "youtube";
-        Sub(): $mol_image | $mol_embed_native | $mol_embed_youtube;
+        sub(): $mol_image[] | $mol_embed_youtube[] | $mol_embed_native[];
     }
 }
 

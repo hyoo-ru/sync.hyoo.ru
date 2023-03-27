a1324f087a68c98990a89d0ccf347a6085716e13
nin-jin <nin-jin@users.noreply.github.com>
2023-03-27 07:17:19 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- watch/web.js
+++ watch/web.js
@@ -10659,11 +10659,14 @@ var $;
 var $;
 (function ($) {
     class $mol_button_copy extends $mol_button_minor {
+        blobs() {
+            return [
+                this.text_blob(),
+                this.html_blob()
+            ];
+        }
         data() {
-            return {
-                "text/plain": this.text_blob(),
-                "text/html": this.html_blob()
-            };
+            return {};
         }
         sub() {
             return [
@@ -10740,6 +10743,9 @@ var $;
     var $$;
     (function ($$) {
         class $mol_button_copy extends $.$mol_button_copy {
+            data() {
+                return Object.fromEntries(this.blobs().map(blob => [blob.type, blob]));
+            }
             html() {
                 return $mol_html_encode(this.text());
             }

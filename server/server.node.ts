cb0f8f9e4062f7187f2bad47f32aeee1a4990863
jin <jin@hyoo.ru>
2023-04-02 12:29:45 +0300

    +1

--- server/server.node.ts
+++ server/server.node.ts
@@ -253,10 +253,10 @@ namespace $ {
 							h1, h2, h3, h4, h5, h6, p {
 								margin: 0;
 								padding: .5rem .75rem;
+								line-height: 1.5rem;
 							}
 							
 							a {
-								padding: 0.5rem 0.75rem;
 								text-decoration: none;
 							}
 							

67514af512e2f671be2f40f16599d5b00ab97d94
nin-jin <sairi-na-tenshi@ya.ru>
2023-11-28 17:35:43 +0300

    Update server.node.ts

--- server/server.node.ts
+++ server/server.node.ts
@@ -313,7 +313,9 @@ namespace $ {
 									if( !name ) return ''
 									const tag = name.replace( /_.*$/, '' )
 									return [
-										`<${tag} id="land=${id}=(${name})">`,
+										Array.isArray( value ) && $mol_int62_string_ensure( value[0] ) )
+											? `<${tag} id="land=${id}=(${name})" hidden>`
+											: `<${tag} id="land=${id}=(${name})">`,
 										value,
 										`</${tag}>`,
 									]

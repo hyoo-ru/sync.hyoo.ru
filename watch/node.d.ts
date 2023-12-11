08313dfb5d35e488401471928f3d8601d4824c0f
nin-jin <nin-jin@users.noreply.github.com>
2023-12-11 07:18:00 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- watch/node.d.ts
+++ watch/node.d.ts
@@ -2192,7 +2192,13 @@ declare namespace $.$$ {
 }
 
 declare namespace $ {
-    class $mol_icon_github_circle extends $mol_icon {
+    class $mol_icon_script extends $mol_icon {
+        path(): string;
+    }
+}
+
+declare namespace $ {
+    class $mol_icon_script_text extends $mol_icon {
         path(): string;
     }
 }
@@ -2201,7 +2207,7 @@ declare namespace $ {
     class $mol_link_source extends $mol_link {
         hint(): string;
         sub(): readonly any[];
-        Icon(): $mol_icon_github_circle;
+        Icon(): $mol_icon_script_text;
     }
 }
 
